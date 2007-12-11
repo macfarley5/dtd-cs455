a9678034906030eefb0249fd90b63d9dc21b7fd1
josephbut <josephbut@d31d6850-ee3f-0410-b7f4-09c8ee69ac4e>
2007-12-11 06:06:01 +0000

    almost have the bugs out

--- WindowsGame1/WindowsGame1/PathPlanner.cs
+++ WindowsGame1/WindowsGame1/PathPlanner.cs
@@ -15,7 +15,7 @@ namespace TD3d
         /// Custom comparer for the SortedList Fringe
         /// </summary>
         /// 
-        Map myMap;
+        
         public class nodeComparer : IComparer
         {
             int System.Collections.IComparer.Compare(Object firstv, Object secondv)
@@ -54,8 +54,9 @@ namespace TD3d
         private int endX;
         private int depth;
         private int width;
+        private Map myMap;
 
-        public PathPlanner(int inWidth, int inDepth, int inStartX, int inStartZ, int inEndX, int inEndZ)
+        public PathPlanner(int inWidth, int inDepth, int inStartX, int inStartZ, int inEndX, int inEndZ, Map inMap)
         {
             path = new ArrayList();
             visited = new Queue();
@@ -63,138 +64,149 @@ namespace TD3d
             startZ = inStartZ;
             endX = inEndX;
             endZ = inEndZ;
-            width = inWidth;
-            depth = inDepth;
+            width = inWidth - 1;
+            depth = inDepth - 1;
+            myMap = inMap;
         }
 
         private bool canMove(Node inNode, string dir)
         {
-            bool moveAllowed = true;
-            if (dir == "up")
+            bool moveAllowed = false;
+            if ((width >= inNode.X && inNode.X >= 0) && (depth >= inNode.Z && inNode.Z >= 0))
             {
-                if (inNode.Z + 1 >= depth)
+                moveAllowed = true;
+                switch (dir)
                 {
-                    moveAllowed = false;
+                    case "up":
+                        if (inNode.Z + 1 >= depth)
+                        {
+                            moveAllowed = false;
+                        }
+                        else if (myMap.isOccupied(inNode.X, inNode.Z + 1))
+                        {
+                            moveAllowed = false;
+                        }
+                        break;
+                    case "down":
+                        if (inNode.Z - 1 < 0)
+                        {
+                            moveAllowed = false;
+                        }
+                        else if (myMap.isOccupied(inNode.X, inNode.Z - 1))
+                        {
+                            moveAllowed = false;
+                        }
+                        break;
+                    case "left":
+                        if (inNode.x - 1 < 0)
+                        {
+                            moveAllowed = false;
+                        }
+                        else if (myMap.isOccupied(inNode.X - 1, inNode.Z))
+                        {
+                            moveAllowed = false;
+                        }
+                        break;
+                    case "right":
+                        if (inNode.X + 1 >= width)
+                        {
+                           moveAllowed = false;
+                        }
+                        else if (myMap.isOccupied(inNode.X + 1, inNode.Z))
+                        {
+                            moveAllowed = false;
+                        }
+                        break;
                 }
-                if (myMap.isOccupied(inNode.X, inNode.Z - 1))
-                {
-                    moveAllowed = false;
-                }
-            }
-            else if (dir == "down")
-            {
-                if (inNode.Z - 1 <= depth)
-                {
-                    moveAllowed = false;
-                }
-                if (myMap.isOccupied(inNode.X, inNode.Z - 1))
-                {
-                    moveAllowed = false;
-                }
-            }
-            else if (dir == "left")
-            {
-                if (inNode.x - 1 <= width)
-                {
-                    moveAllowed = false;
-                }
-                if (myMap.isOccupied(inNode.X - 1, inNode.Z))
-                {
-                    moveAllowed = false;
-                }
-            }
-            else if (dir == "right")
-            {
-                if (inNode.X + 1 >= width)
-                {
-                    moveAllowed = false;
-                }
-                if (myMap.isOccupied(inNode.X + 1, inNode.Z))
-                {
-                    moveAllowed = false;
-                }
-            }
-            else
-            {
-                moveAllowed = false;
             }
             return moveAllowed;
         }
 
         public bool beenVisited(Node inNode)
         {
-            return visited.Contains(inNode);
+            bool retVal = false;
+            if (visited.Count != 0)
+            {
+                foreach (Node tempy in visited)
+                {
+                    if (tempy.X == inNode.X && tempy.Z == inNode.Z)
+                    {
+                        retVal = true;
+                    }
+                }
+                //System.Console.WriteLine("check visited for " + inNode.X + ", " + inNode.Z);
+            }
+            return retVal;
         }
 
         private ArrayList findPath()
         {
             IComparer myComparison = new nodeComparer();
             ArrayList fringe = new ArrayList();
-            Node currentNode = new Node(startX, startZ, null);
-            Node goalNode = new Node(endX, endZ, null);
+            Node currentNode = new Node(startX + 1, startZ, null);
+            Node goalNode = new Node(endX - 1, endZ - 1, null);
+            endX--;
+            endX--;
+            //System.Console.WriteLine("goal node = " + endX + ", " + endZ);
             //start at the beginning and go to the end
             bool blocked = false;
-            bool left = false;
-            bool right = false;
-            bool up = false;
-            bool down = false;
+
             while (currentNode != goalNode)
             {
-                left = true;
-                right = true;
-                up = true;
-                down = true;
                 int newX = currentNode.X;
                 int newZ = currentNode.Z;
                 if (canMove(currentNode, "up"))
                 {
-                    newZ++;
-                    Node temp = new Node(newX, newZ, currentNode);
+                    Node temp = new Node(newX, newZ + 1, currentNode);
                     if (!beenVisited(temp))
                     {
                         temp.Cost = Math.Abs(endZ - newZ);
                         temp.CostToGo = endX - newX;
-                        visited.Enqueue(temp);
+                        //visited.Enqueue(temp);
                         fringe.Add(temp);
+                        //System.Console.WriteLine(newX + ", " + (newZ + 1) + " added to fringe up");
                     }
                 }
-                else if (canMove(currentNode, "down"))
+                if (canMove(currentNode, "down"))
                 {
-                    newZ--;
-                    Node temp = new Node(newX, newZ, currentNode);
+                    Node temp = new Node(newX, newZ - 1, currentNode);
                     if (!beenVisited(temp))
                     {
-                        temp.Cost = Math.Abs(endZ - newZ);
+                        temp.Cost = Math.Abs(endZ - newZ - 1);
                         temp.CostToGo = endX - newX;
-                        visited.Enqueue(temp);
+                        //visited.Enqueue(temp);
                         fringe.Add(temp);
+                        //System.Console.WriteLine(newX + ", " + (newZ - 1) + " added to fringe down");
                     }
                 }
-                else if (canMove(currentNode, "left"))
+                if (canMove(currentNode, "left"))
                 {
-                    newX--;
-                    Node temp = new Node(newX, newZ, currentNode);
+                    //newX--;
+                    Node temp = new Node(newX - 1, newZ, currentNode);
                     if (!beenVisited(temp))
                     {
                         temp.Cost = Math.Abs(endZ - newZ);
-                        temp.CostToGo = endX - newX;
-                        visited.Enqueue(temp);
+                        temp.CostToGo = endX - newX - 1;
+                        //visited.Enqueue(temp);
                         fringe.Add(temp);
+                        //System.Console.WriteLine((newX - 1) + ", " + newZ + " added to fringe left");
                     }
                 }
-                else if (canMove(currentNode, "right"))
+                if (canMove(currentNode, "right"))
                 {
-                    newX++;
-                    Node temp = new Node(newX, newZ, currentNode);
+                    //newX++;
+                    Node temp = new Node(newX + 1, newZ, currentNode);
                     if (!beenVisited(temp))
                     {
                         temp.Cost = Math.Abs(endZ - newZ);
-                        temp.CostToGo = endX - newX;
-                        visited.Enqueue(temp);
+                        temp.CostToGo = endX - newX + 1;
+                        //visited.Enqueue(temp);
                         fringe.Add(temp);
+                        //System.Console.WriteLine((newX + 1) + ", " + newZ + " added to fringe right");
                     }
                 }
                 int size = fringe.Count;
+                //System.Console.WriteLine(size);
                 if (size == 0)
                 {
                     blocked = true;
@@ -202,6 +214,7 @@ namespace TD3d
                 }
                 fringe.Sort(myComparison);
                 currentNode = (Node)fringe[0];
+                visited.Enqueue(currentNode);
                 fringe.RemoveAt(0);
             }
             if (blocked)
@@ -210,6 +223,7 @@ namespace TD3d
             }
             else
             {
+                System.Console.WriteLine("The path that exists is " + path.Count + " nodes long");
                 while (currentNode != null)
                 {
                     path.Add(currentNode);
@@ -220,7 +234,7 @@ namespace TD3d
             return path;
         }
 
-        bool isPath()
+        public bool isPath()
         {
             ArrayList possiblePath = findPath();
             if (possiblePath == null)
@@ -230,8 +244,17 @@ namespace TD3d
             return true;
         }
 
-        ArrayList getPath()
+        public ArrayList getPath()
         {
+            if (!isPath())
+            {
+                path = new ArrayList();
+                Node temp = new Node(startX, startZ, null);
+                path.Add(temp);
+                Node temp1 = new Node(endX, endZ, null);
+                temp1.Parent = temp;
+                path.Add(temp1);
+            }
             return path;
         }
     }
