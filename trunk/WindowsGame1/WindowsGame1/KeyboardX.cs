43931b869333e44e9fdaa26e17d6b78f6d00eb30
hansede <hansede@d31d6850-ee3f-0410-b7f4-09c8ee69ac4e>
2007-12-14 22:25:54 +0000

    Oops, they're actually added now

---
+++ trunk/WindowsGame1/WindowsGame1/KeyboardX.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using System.IO;
+
+namespace TD3d
+{
+    class KeyboardX
+    {
+        int WIDTH;
+        Vector3 cameraRot;
+        float cameraDist;
+
+        public KeyboardX(int WIDTH)
+        {
+            this.WIDTH = WIDTH;
+        }
+
+        public void update(Vector3 cameraRot, float cameraDist, float speed)
+        {
+            KeyboardState keys = Keyboard.GetState();
+            
+            if (keys.IsKeyDown(Keys.Right))
+                cameraRot.X += .05f;
+            if (keys.IsKeyDown(Keys.Left))
+                cameraRot.X -= .05f;
+            if (keys.IsKeyDown(Keys.Up))
+            {
+                cameraRot.Z += .02f;
+
+                if (cameraRot.Z > 1.5f)
+                    cameraRot.Z = 1.5f;
+            }
+            if (keys.IsKeyDown(Keys.Down))
+            {
+                cameraRot.Z -= .02f;
+
+                if (cameraRot.Z < 0.2f)
+                    cameraRot.Z = 0.2f;
+            }
+            if (keys.IsKeyDown(Keys.S))
+            {
+                cameraDist += .5f;
+
+                if (cameraDist > this.WIDTH * 3)
+                    cameraDist = this.WIDTH * 3;
+            }
+            if (keys.IsKeyDown(Keys.W))
+            {
+                cameraDist -= .5f;
+
+                if (cameraDist < 5.25f)
+                    cameraDist = 5.25f;
+            }
+
+            this.cameraRot = cameraRot;
+            this.cameraDist = cameraDist;
+        }
+
+        public Vector3 getCameraRot()
+        {
+            return this.cameraRot;
+        }
+
+        public float getCameraDist()
+        {
+            return this.cameraDist;
+        }
+    }
+}
